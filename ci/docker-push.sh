8e39f8406e552a717c7a15dc9a8cec15e30bb070
Aaftakhov <simple1elf@gmail.com>
2021-07-09 17:11:44 +0500

    fix docker username

--- ci/docker-push.sh
+++ ci/docker-push.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-docker build -t $GITHUB_REPOSITORY .
-docker tag $GITHUB_REPOSITORY $GITHUB_REPOSITORY:$1
+docker build -t simple1elf/selenoid .
+#docker tag $GITHUB_REPOSITORY $GITHUB_REPOSITORY:$1
+docker tag simple1elf/selenoid simple1elf/selenoid:$1
 #docker tag $GITHUB_REPOSITORY selenoid/hub:$1
 #docker tag $GITHUB_REPOSITORY aandryashin/selenoid:$1
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push $GITHUB_REPOSITORY
-docker push $GITHUB_REPOSITORY:$1
+docker push simple1elf/selenoid
+docker push simple1elf/selenoid:$1
 #docker push selenoid/hub:$1
 #docker push aandryashin/selenoid:$1
-#push
